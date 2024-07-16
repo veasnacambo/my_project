bc19d5a022e39098cbc8d267a5928c7d03d49232
veasnacambo <117183980+veasnacambo@users.noreply.github.com>
2024-07-16 13:52:25 +0700

    Update main.c
    update main.c

--- src/main.c
+++ src/main.c
@@ -2,16 +2,7 @@
 #include "que_packet_handle.h"
 #include "neighbor_table.h"
 usr_data_info_t UserDataC  = {0};
-add_node_to_group_info_t datay = {0};
-uint8_t us[14];
-NeighborInfo_t gg;
 int main(){
-
-    datay.group_chat_id = 0x00AB;
-    datay.adding_node.ID= 0x00BA;
-    strcpy(datay.adding_node.Name,"ABDC");
-    gg.ID = 0xEA62;
-    // add_nodeINF_Uint8t_convert(datay,UserDataC.data);
 
     // initialize nvs-storage
     nvs_memo_init();
